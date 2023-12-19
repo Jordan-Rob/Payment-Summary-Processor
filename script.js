e81acf3ec26ba3a6172485e328b8ccbdb21eeba8
Jordan Rob <jordanrob709@gmail.com>
2023-12-19 13:10:19 +0300

    Modified script to use Streams and Readline for improved performance

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const fs = require("fs").promises; // import fs with promise-based interfaces to allow asynchronous functionality
+const fs = require("fs"); // import fs with promise-based interfaces to allow asynchronous functionality
+const readline = require("readline")
 
 const args = process.argv.slice(2); // retrieve array containing options entered when running the script in CLI
 const inputFileArg = args[0] || "input.csv"; // declare variable for inputfile option entered in CLI if provided or use default
@@ -11,52 +12,51 @@ async function processFile(
   outputFile = outputFileArg
 ) {
   try {
-    const content = await fs.readFile(inputFile, "utf8"); // read content from inputFile
-    if (content.includes(",")) {
-      // check for correct delimeter, comma(,)
-      const lines = content.split("\n"); // split each line in inputfile content and save them in an array
+    // create readable stream for input file 
+    const inputStream = fs.createReadStream(inputFile, { encoding: "utf8" })
 
-      lines.forEach((line) => {
-        const [payer, payee, amount] = line.split(","); // iterate through each line destructing values for payer. payee and amount
-        const key = `${payer}_${payee}`; // create a key that is a string combination of payer and payee
+    // Create a readline interface for reading lines from the stream
+    const rl = readline.createInterface({ input: inputStream });
 
-        if (!isNaN(parseFloat(amount))) {
-          data[key] = (data[key] || 0) + parseFloat(amount); // save cummulative amount as value of the key created above and store it in data object
-        }
-      });
+    // Process each line using the stream
+    rl.on("line", (line) => {
+      const [payer, payee, amount] = line.split(",")
+      const key = `${payer}_${payee}`
 
-      await summarizeData(data, outputFile); // run summarizeData with the populated data object and outputFile declared
-    } else {
-      // error will be thrown if values in the input file content are not separed by comma
-      throw Error(
-        "Invalid file format: Please use a CSV with comma-separated values."
-      );
-    }
+      if (!isNaN(parseFloat(amount))) {
+        data[key] = (data[key] || 0) + parseFloat(amount)
+      }
+    })
+
+    // wait for stream to end
+    await new Promise((resolve) => {
+      rl.on("close", resolve);
+    })
+
+    // Output processed data
+    await summarizeData(data, outputFile)
+
   } catch (err) {
     // function will throw an error if there was an issue reading the input file
-    throw Error("Error reading input file");
+    console.error("Error reading input file:", err.message);
   }
 }
 
-async function summarizeData(data) {
-  const outputLines = []; // declare array to store content lines to be wriiten to outputFile
+async function summarizeData(data, outputFile) {
+  const outputStream = fs.createWriteStream(outputFile, { encoding: "utf8" })
+  //const outputLines = []; // declare array to store content lines to be wriiten to outputFile
 
   for (const key in data) {
     const [payer, payee] = key.split("_"); // iterate over keys in data object and destructure values of payer and payee
     const amount = parseFloat(data[key]).toFixed(2); // retrieve amount value with 2 decimal places in string format
-    outputLines.push(`${payer},${payee},${amount}`); // create a string of payer,payee,amount and save it as an item in outputLines array
+    //outputLines.push(`${payer},${payee},${amount}`);
+    outputStream.write(`${payer},${payee},${amount}\n`);
   }
 
-  const outputContent = outputLines.join("\n"); // create output content from outputLines array writing each item on a new line
+  outputStream.end();
+  console.log(`Summary saved to ${outputFile}`);
 
-  try {
-    await fs.writeFile(outputFileArg, outputContent); // write outputContent to specified outputfile
-    //console.log(`Summary saved to ${outputFileArg}`)
-  } catch (error) {
-    throw error;
-  }
 }
 
 processFile();
-
 module.exports = { summarizeData, processFile };
